a376c1cb406d701a782999e6ac500961a2646879
JesusLeon <jesusalbertoleonjimenez@gmail.com>
2024-07-05 13:51:23 -0500

    Primer commit

---
+++ src/blackjack/usecases/pedir-carta.js
@@ -0,0 +1,14 @@
+/**
+ * Esta función me permite tomar una carta de la baraja
+ * @param {Array<String>} deck es un arreglo de string
+ * @returns {String} retorna la carta de la baraja
+ */
+
+export const pedirCarta = ( deck ) => {
+
+    if ( !deck || deck.length === 0 ) {
+        throw 'No hay cartas en el deck';
+    }
+    const carta = deck.pop();
+    return carta;
+}
